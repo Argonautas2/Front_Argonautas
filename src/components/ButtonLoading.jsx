a4bd43e17997f794765433c93dc7e9b542a7252e
unknown <sandracaballero11@gmail.com>
2021-12-15 12:33:46 -0500

    modifiaciones para generar pruebas unitarias

--- src/components/ButtonLoading.jsx
+++ src/components/ButtonLoading.jsx
@@ -11,7 +11,7 @@ const ButtonLoading = ({ disabled, loading, text }) => {
       type='submit'
       className='bg-indigo-700 text-white font-bold text-lg py-3 px-6  rounded-xl hover:bg-indigo-500 shadow-md my-2 disabled:opacity-50 disabled:bg-gray-700'
     >
-    {loading ? <ReactLoading data-testid="loading-in-button" type='spin' height={30} width={30} /> : text}
+    {loading ? <ReactLoading type='spin' height={30} width={30} /> : text}
     </button>
   );
 };
