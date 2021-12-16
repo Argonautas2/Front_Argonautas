9ccd4d161a34de89ea7cd8aff097d7dda20634da
unknown <sandracaballero11@gmail.com>
2021-12-15 21:14:35 -0500

    modificacion Index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Index = () => {
   return (
-    <div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-    </div>
+    <div className='py-2 w-full flex flex-col items-center justify-center'>
+    <img src='logo.png' alt='Logo' className='h-26' />
+    <span className='my-2 text-xl font-bold text-center'>ARGONAUTAS GESTION DE PROYECTOS</span>
+  </div>
   );
 };
 
 export default Index;
+
+
+
+  
+    
+  
